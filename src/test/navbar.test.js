1d224d0031c69f79c22e89815812ebb4137d01fe
mathsoft29 <iwuconcept@gmail.com>
2023-08-26 17:42:26 +0100

    Fix linters

--- src/test/navbar.test.js
+++ src/test/navbar.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../components/navbar';
+
 describe('Navbar', () => {
   test('clicking back button invokes window.history.back()', () => {
     window.history.back = jest.fn();
     render(
       <BrowserRouter>
         <Navbar />
-      </BrowserRouter>
+      </BrowserRouter>,
     );
     const backButton = screen.getByLabelText('Button with Icon');
     fireEvent.click(backButton);
